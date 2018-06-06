753550c033a8307d3f6124b8331d4b1bacf133f5
Abhinav Sharma <abhinavsharma13@Abhinavs-MacBook-Air.local>
2018-06-06 10:47:44 +0530

    adding second test case

--- core/module.py
+++ core/module.py
@@ -2,3 +2,6 @@ class test1():
 
     def test_fn(self):
         return 9 * 10
+
+    def test_fn2(self):
+        return 10 * 10
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,3 +5,7 @@ import pytest
 def test_case1():
     obj = core.module.test1()
     assert obj.test_fn() == 90
+
+def test_case2():
+    obj = core.module.test1()
+    assert obj.test_fn2() == 100
